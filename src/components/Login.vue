7e325502bab45166246a4674c693dc85f4962234
noerrornowork <gao.yang@transn.com>
2019-11-25 18:29:48 +0800

    哈哈哈

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login_container">
+    登陆组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  height: 100%;
+  background-color: #2b4b6b;
+}
+</style>
