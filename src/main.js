f62ed84e88fd981d5fc68275933ab5048f1314be
noerrornowork <gao.yang@transn.com>
2019-11-26 18:44:01 +0800

    完成了用户列表的开发

--- src/main.js
+++ src/main.js
@@ -11,6 +11,11 @@ import './assets/css/global.css'
 import $axios from 'axios'
 // 配置请求的根路径
 $axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 配置axios请求拦截器权限字段
+$axios.interceptors.request.use(config => {
+  config.headers.Authorization = sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$axios = $axios
 
 Vue.config.productionTip = false
